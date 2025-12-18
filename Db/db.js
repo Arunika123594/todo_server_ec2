23d43857e6c85bdc85b01f79982ea950e663c301
Arunika123594 <arunika.n2024csbs@sece.ac.in>
2025-12-18 14:01:44 +0530

    first

--- Db/db.js
+++ Db/db.js
@@ -1,17 +1,14 @@
-// we will handle connectivity between express and mongodb
-//const mongoose =require('mongoose')......for commonjs
+// we will handle connectivity between express and mongodb //const mongoose =require('mongoose')......for commonjs 
 import mongoose from 'mongoose'
-
 import dotenv from "dotenv";
 dotenv.config();
-
-
-const connectDb=()=>{
-try{
-    mongoose.connect(process.env.MONGODB_URL);
-    console.log("db has been connected");
-}catch(err){
-    console.error(err)
-}
+const connectDb = () => {
+    try {
+        mongoose.connect(process.env.MONGO_URI);
+        console.log("db has been connected");
+    }
+    catch (err) {
+        console.error(err)
+    }
 }
 export default connectDb
